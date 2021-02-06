a82f46371faf9b94359b4ffb9a1b4c8ce42f4239
John Nkp <john.nkp1@gmail.com>
2021-02-07 07:22:52 +1100

    added video background to landing page

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module PortfolioV2
   class Application < Rails::Application
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -18,5 +19,6 @@ module PortfolioV2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
   end
 end
